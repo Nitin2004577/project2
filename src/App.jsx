40866301d050b7d9ef586cd6dd8c2d1c65632681
Nitin2004577 <shahnitin500@gmail.com>
2024-07-16 09:52:51 +0545

    home and Destination page

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+import UserLayout from "./layouts/UserLayout";
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/user" element={<UserLayout />}>
+            <Route path="home" element={<Home />} />
+            <Route path="destination" element={<Destination />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
